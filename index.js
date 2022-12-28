2dfa30212189b80a6c03e99fd72b529cb887a2a4
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-12-28 19:22:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+//To holding users information 
+const socketsStatus = {};
+
+//config and set handlebars to express
+const customHandlebars = handlebars.create({ layoutsDir: "./views" });
+
+app.engine("handlebars", customHandlebars.engine);
+app.set("view engine", "handlebars");
+
+//enable user access to public folder 
+app.use("/files", express.static("public"));
+
+app.get("/",(req,res)=>{
+  res.status(200).json({
+    message:"working fine."
+  });
+})
+
+app.get("/home" , (req , res)=>{
+    res.render("index");
+});
+
+app.get("*",(req,res)=>{
+    res.render("404");
+})
+
+io.on("connection", function (socket) {
+  const socketId = socket.id;
+  socketsStatus[socket.id] = {};
+
+
+  console.log("connect");
+
+  socket.on("voice", function (data) {
+
+    var newData = data.split(";");
+    newData[0] = "data:audio/ogg;";
+    newData = newData[0] + newData[1];
+
+    for (const id in socketsStatus) {
+
+      if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
+        socket.broadcast.to(id).emit("send", newData);
+    }
+
+  });
+
+  socket.on("userInformation", function (data) {
+    socketsStatus[socketId] = data;
+
+    io.sockets.emit("usersUpdate",socketsStatus);
+  });
+
+
+  socket.on("disconnect", function () {
+    delete socketsStatus[socketId];
+  });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log("Server Active on PORT 3000!");
+});
